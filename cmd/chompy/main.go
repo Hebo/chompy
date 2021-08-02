6fc496e258c402434c482fb5104f0ebe5cf9b92f
Hebo <hebo@hey.com>
2021-08-02 16:58:53 -0700

    add pprof

--- cmd/chompy/main.go
+++ cmd/chompy/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/caarlos0/env/v6"
 	"github.com/hebo/chompy/config"
 	"github.com/hebo/chompy/server"
@@ -14,6 +17,11 @@ func main() {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalln("Failed to parse config", err)
 	}
+
+	go func() {
+		// Pprof
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	server := server.New(cfg, afero.NewOsFs())
 	server.Serve(cfg.Port)
