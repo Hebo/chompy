ac3658b68b71d1402f452a0e2e5acc2c60cb7d68
Hebo <hebo@hey.com>
2022-04-23 07:57:24 +0200

    Don't download live chat

--- downloader/options.go
+++ downloader/options.go
@@ -46,6 +46,7 @@ func defaultOptions() ytdlopts {
 		boolOption{"--no-mtime"},
 		boolOption{"--no-playlist"},
 		boolOption{"--embed-subs"},
+		stringOption{"--sub-langs", "all,-live_chat"},
 		boolOption{"--netrc"},
 		stringOption{"--match-filter", "!is_live & !was_live"},
 		stringOption{"--sponsorblock-remove", "sponsor"},
